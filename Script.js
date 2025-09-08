3a0a498b448c2df3cd58f65698a9b7578084fb66
Adrian <adrian52507@gmail.com>
2025-09-08 00:58:40 -0500

    Todo el dia para esta animacion de intro a la pagina

--- Script.js
+++ Script.js
@@ -1,51 +1,96 @@
 // Ocultamos el texto de inicio
-gsap.set(".titulo", { opacity: 0 });
+gsap.set(".titulo", { display: "inline", opacity: 0 });
 
 // Timeline principal
-let tl = gsap.timeline({ repeat: 0, yoyo: false });
+let tl = gsap.timeline({ repeat: 0, yoyo: false, paused: true });
 
+// Reproducir sonido
+let audio_swoosh = document.getElementById("titulo-sonido");
+let audio_comenzar = document.getElementById("comenzar-sonido");
+let audio_coin = document.getElementById("moneda-sonido");
+  
 // 1. Efecto de entrada para la pelota de fútbol
 tl.from("#futbol", {
-  duration: 1,
+  duration: 0.8,
   scale: 0,         // empieza pequeño
   opacity: 0,       // invisible
   transformOrigin: "50% 50%", // centro del svg
-  ease: "back.out(1.7)"
+  ease: "back.out(1.5)",
+  onStart: () => {
+    audio_swoosh.currentTime = 0;
+    audio_swoosh.play();
+  }
 });
 
 // 2. Morph a tennis
 tl.to("#futbol", {
-  duration: 1,
+  duration: 0.8,
   morphSVG: "#tennis",
-  ease: "power2.inOut"
+  ease: "power2.inOut",
+  onStart: () => {
+    audio_swoosh.currentTime = 0;
+    audio_swoosh.play();
+  }
 });
 
 // 3. Morph a banca
 tl.to("#futbol", {
-  duration: 1,
+  duration: 0.8,
   morphSVG: "#banca",
-  ease: "power2.inOut"
+  ease: "power2.inOut",
+  onStart: () => {
+    audio_swoosh.currentTime = 0;
+    audio_swoosh.play();
+  }
 });
 
 // 4. Morph a basquet
 tl.to("#futbol", {
-  duration: 1,
+  duration: 0.8,
   morphSVG: "#basquet",
-  ease: "power2.inOut"
+  ease: "power2.inOut",
+  onStart: () => {
+    audio_swoosh.currentTime = 0;
+    audio_swoosh.play();
+  }
 });
 
 // 5. Morph a mancuerna
 tl.to("#futbol", {
-  duration: 1,
+  duration: 0.8,
   morphSVG: "#mancuerna",
   ease: "power2.inOut",
+  onStart: () => {
+    audio_swoosh.currentTime = 0;
+    audio_swoosh.play();
+  },
   onComplete: () => {
     // mostrar el texto BET-SPORTS al terminar
-    gsap.to(".titulo", { opacity: 1, duration: 1, delay: 0.5 });
-  
-    // Reproducir sonido
-    let audio = document.getElementById("titulo-sound");
-    audio.muted = false;
-    audio.play();
+    gsap.to(".titulo", { opacity: 1, duration: 1 });
+    audio_coin.play();
   }
 });
+
+document.getElementById("boton").addEventListener("click", () => {
+  // Ocultar el botón con animación
+  gsap.to("#boton", {
+    opacity: 0,
+    scale: 1,
+    duration: 0.2,
+    transformOrigin: "50% 50%",
+    ease: "power2.inOut",
+    onStart: () => {
+      audio_comenzar.currentTime = 0;
+      audio_comenzar.play();
+    },
+    onComplete: () => {
+      // Quitarlo del flujo para que no bloquee clics
+      gsap.set("#boton", { display: "none" });
+      gsap.set("#futbol", { display: "inline" });
+
+      // Reproducir timeline
+      tl.play();
+    }
+  });
+});
+
